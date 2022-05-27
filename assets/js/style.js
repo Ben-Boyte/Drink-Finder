0f53fc60912bf6f3d8ab82c86175cf09c2b9e3c1
Ben-Boyte <zissette1@gmail.com>
2022-05-27 12:23:22 -0500

    added click events for images and made images buttons

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+var cocktailImg = document.getElementById("cocktailImg");
+var brewImg = document.getElementById("breweryImg");
+
+
+brewImg.addEventListener("click", () => {
+    console.log("click");
+});
+
+cocktailImg.addEventListener("click", () => {
+    console.log("click");
+});
