23b4324efa4b8da0e4bb25ba2e9711e5f265a4bb
connermtodd <99088964+connermtodd@users.noreply.github.com>
2022-06-02 21:05:22 -0400

    displayed data from API onto page

--- assets/js/cocktail.js
+++ assets/js/cocktail.js
@@ -1,32 +1,33 @@
-const API_key = 1;
+const clickButton = document.querySelector("#cocktail-button").addEventListener("click", (findCocktail));
 
-var randomCocktail = function() {
-    fetch("https://thecocktaildb.com/api/json/v1/1/random.php");
+    
+    function findCocktail() {
+    
+    const APIkey = 1;
+    var cocktailFinder = "https://thecocktaildb.com/api/json/v1/1/random.php";
+
+    fetch(cocktailFinder)
+
+    .then(response => response.json()).then(data => {
+        
+         const cocktail = (data.drinks[0].strDrink);
+         const cocktailRecipe = (data.drinks[0].strInstructions);
+        console.log(cocktail, cocktailRecipe);
+
+        const drink = document.getElementById('drink');
+        const drinkEl = document.createElement('div');
+        const titleEl = document.createElement('span');
+        titleEl.textContent = cocktail;
+        drinkEl.appendChild(titleEl);
+        drink.appendChild(drinkEl);
+
+        const recipe = document.getElementById('recipe');
+        const recipeEl = document.createElement('div');
+        const spanEl = document.createElement('span');
+        spanEl.textContent = cocktailRecipe;
+        recipeEl.appendChild(spanEl);
+        recipe.appendChild(recipeEl);
+
+
+    })
 };
-
-document.querySelector("#cocktail-button").addEventListener("click", function(event) {
-
-    const cocktailFinder = "https://thecocktaildb.com/api/json/v1/1/random.php";
-
-    fetch(cocktailFinder);
-
-    console.log(randomCocktail);
-
-   });
-
-var displayCocktail = function(){
-   
-    for (var i = 0; i < cocktail.length; i++) {
-
-    var cocktailName = cocktail[i].strDrink
-    
-    var cocktailEl = document.createElement("div");
-    var titleEl = document.createElement("span");
-    
-    titleEl.textContent = cocktailName;
-    
-    cocktailEl.appendChild(titleEl);
-    
-    cocktailContainerEl.appendChild(cocktailEl);
-   };
-}
